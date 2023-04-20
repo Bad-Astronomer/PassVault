c28cdabe4499846718c0c5a2c1b2a743b19fd37c
Bad-Astronomer <kedarmakulkarni@gmail.com>
2023-04-20 09:58:06 +0530

    Modified .features

--- frontend/home_main/home-script.js
+++ frontend/home_main/home-script.js
@@ -10,5 +10,20 @@ const mouse_move = event => {
 }
 
 for(const feature of document.querySelectorAll(".features")){
-    feature.onmousemove = event => mouse_move(event)
+    feature.onmousemove = event => mouse_move(event);
+}
+
+const feature_para = document.querySelectorAll(".features p");
+window.onresize = () => {
+    let height = feature_para[0].getBoundingClientRect().height;
+    for(let i = 0; i < feature_para.length; i++){
+        feature_para[i].style.height = "fit-content";
+        let feature_height = feature_para[i].getBoundingClientRect().height;
+        if (feature_height > height) height = feature_height;
+    }
+    setTimeout(() => {
+        for(let i = 0; i < feature_para.length; i++){
+            feature_para[i].style.height = `${height}px`;
+        }
+    }, 500);
 }
